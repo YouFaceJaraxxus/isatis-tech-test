c4bd45c6a8678f02b1e42b42fbcf94202988ba9b
MLukicJSG <milos.lukic@jsguru.io>
2022-04-22 22:35:46 +0200

    Pulled the <layout> outside the logic in order to avoid repetitive code, moved the guard logic into a function.

--- src/routes/CustomRoute.jsx
+++ src/routes/CustomRoute.jsx
@@ -3,34 +3,31 @@ import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
 import Home from '../components/home/Home';
 import Layout from '../components/layout/Layout';
+import Login from '../components/login/Login';
+import Recipes from '../components/recipes/Recipes';
 
 
 const CustomRoute = ({ path, exact, component: RouteComponent }) => {
 
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const renderComponent = () => {
+    if(!isLoggedIn && path !== '/' && path !== '/login'){
+      return <Login />
+    }else if(isLoggedIn && path === '/login'){
+      return <Recipes />
+    }else return <RouteComponent />
+  }
+
   return (
     <Route
       path={path}
       exact={exact}
-      render={(props) => {
-        if (!isLoggedIn && path !== '/' && path !== '/login') {
-          return (
-            <Layout>
-              <Home />
-            </Layout>
-          );
-        } else if (isLoggedIn && path === '/login') {
-          return (
-            <Layout>
-              <Home />
-            </Layout>
-          );
-        } else return (
-          <Layout>
-            <RouteComponent />
-          </Layout>
-        );
-      }}
+      render={(props) => (
+        <Layout>
+          {renderComponent()}
+        </Layout>
+      )}
     />
   );
 }
