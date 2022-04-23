b07bf475f9e3a579ad7200269cc85e5f74d7e1f9
Nemanja Vrljanovic <nvrljanovic@yahoo.com>
2022-04-23 19:40:31 +0200

    Added modal for creating products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -13,9 +13,24 @@ import { openSnackbar } from '../../redux/reducers/commonSlice';
 import { SUCCESS } from '../../common/config/config';
 import Table from '../table/Table';
 import productsClasses from './products.module.scss'
+import Modal from 'react-modal/lib/components/Modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    border: '10px solid'
+  },
+};
 
 const Products = () => {
   const { recipes, fetchingRecipes, currentRecipe } = useSelector((state) => state.recipes);
+
+  const [isOpenModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const sampleRecipe = '-N07b1ZAnPJVFDZMmcpF';
@@ -48,10 +63,43 @@ const Products = () => {
     dispatch(setFetchingRecipes(!fetchingRecipes));
   }
 
-  console.log(recipes);
+  const showCreateProductModal = () => {
+    setOpenModal(true);
+  }
+
+  const closeCreateProductModal = () => {
+    setOpenModal(false);
+  }
+
+  const handleCreateProduct = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+
   return (
     <div className={productsClasses.productsWrapper}>
       <h3>All Products:</h3>
+      <Modal
+        isOpen={isOpenModal}
+        onRequestClose={closeCreateProductModal}
+        style={customStyles}
+      >
+        <h4>Create Product</h4>
+        <form className={productsClasses.createProductForm} onSubmit={handleCreateProduct}>
+          <input type='text' placeholder='Product Name'></input>
+          <input type='text' placeholder='Quantity'></input>
+          <select>
+            <option>Grams</option>
+            <option>Milligrams</option>
+            <option>Kilograms</option>
+            <option>Milliliter</option>
+            <option>Deciliter</option>
+            <option>Liter</option>
+          </select>
+          <input type='submit' value='Create'></input>
+        </form>
+      </Modal>
       <Table data={recipes.map(recipe => {
         return {
             'ID':recipe.id, 
@@ -61,7 +109,7 @@ const Products = () => {
             'Unit': recipe.unit
           };
         })}></Table>
-        <button className={productsClasses.createButton}>Create Product</button>
+        <button className={productsClasses.createButton} onClick={showCreateProductModal}>Create Product</button>
     </div>
   )
 }
