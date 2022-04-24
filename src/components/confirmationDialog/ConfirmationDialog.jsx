32d5d438c4e647327f553a7b00f69ab7b948e39b
MLukicJSG <milos.lukic@jsguru.io>
2022-04-24 11:18:27 +0200

    Completed confirmation dialog.

--- src/components/confirmationDialog/ConfirmationDialog.jsx
+++ src/components/confirmationDialog/ConfirmationDialog.jsx
@@ -1,6 +1,7 @@
-import classNames from 'classnames';
 import React from 'react';
+import { ERROR, SUCCESS } from '../../common/config/config';
 import cDialogClasses from './confirmationDialog.module.scss';
+import classNames from 'classnames/bind';
 
 const CANCEL_DEFAULT_TEXT = 'Cancel';
 const CONFIRM_DEFAULT_TEXT = 'Yes';
@@ -8,17 +9,23 @@ const ConfirmationDialog = ({ isOpen, title, confirmText, cancelText, confirmAct
   const cx = classNames.bind(cDialogClasses);
 
   return isOpen && (
-    <div className={cDialogClasses.confirmationDialogWrapper}>
+    <div className={cx({
+      confirmationDialogWrapper: true,
+      confirmationDialogGreen: type === SUCCESS,
+      confirmationDialogRed: type === ERROR
+    })}>
       <div className={cDialogClasses.title}>{title}</div>
       <div className={cDialogClasses.buttons}>
-        <button className={cx({
-          button: true,
-          confirmButton: true
-        })} onClick={confirmAction}>{confirmText || CONFIRM_DEFAULT_TEXT}</button>
-        <button className={cx({
-          button: true,
+        <div className={cx({
+          confirmationButton: true,
+          confirmButton: true,
+          confirmButtonGreen: type === SUCCESS,
+          confirmButtonRed: type === ERROR,
+        })} onClick={confirmAction}>{confirmText || CONFIRM_DEFAULT_TEXT}</div>
+        <div className={cx({
+          confirmationButton: true,
           cancelButton: true
-        })} onClick={cancelAction}>{cancelText || CANCEL_DEFAULT_TEXT}</button>
+        })} onClick={cancelAction}>{cancelText || CANCEL_DEFAULT_TEXT}</div>
       </div>
     </div>
   )
